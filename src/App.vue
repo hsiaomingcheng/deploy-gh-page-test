89f2fce71ccaef1ea9385cecd293cd08fddfe8ec
Edward <20210603@cyadmk.com>
2022-10-06 13:33:47 +0800

    Add: add SSG

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,17 @@
 <script setup>
+  import { useHead } from '@vueuse/head'
   import axios from 'axios'
   import HelloWorld from './components/HelloWorld.vue'
+
+  useHead({
+    title: 'Website Title',
+    meta: [
+      {
+        name: 'description',
+        content: 'Website description - 火焰哥吉拉',
+      },
+    ],
+  })
 
   const handleSendSlackMsg = () => {
 
@@ -18,18 +29,21 @@
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
+  <client-only>
+    <div>
+      <a href="https://vitejs.dev" target="_blank">
+        <img src="/vite.svg" class="logo" alt="Vite logo" />
+      </a>
+      <a href="https://vuejs.org/" target="_blank">
+        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+      </a>
+    </div>
 
-  <button @click="handleSendSlackMsg">點我發送訊息給Slack</button>
+    <button @click="handleSendSlackMsg">點我發送訊息給Slack</button>
 
-  <HelloWorld msg="Vite + Vue" />
+    <HelloWorld msg="Vite + Vue" />
+
+  </client-only>  
 </template>
 
 <style scoped>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
-import { createApp } from 'vue'
+import { ViteSSG } from 'vite-ssg/single-page'
+// import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// createApp(App).mount('#app')
+
+export const createApp = ViteSSG(App)
